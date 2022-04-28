6004d8f9f307e1f1acae859c54c45bb671388fcf
Michael Ryaboy <sfr.michael@yahoo.com>
2022-04-27 23:56:06 -0700

    erc721 with uri

--- smart-contract/contracts/ERC721.sol
+++ smart-contract/contracts/ERC721.sol
@@ -1,23 +1,38 @@
-pragma solidity ^0.4.23;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-import "../node_modules/zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyERC721 is ERC721Token {
-    constructor (string _name, string _symbol) public
-        ERC721Token(_name, _symbol)
-    {
+contract PrivateEquity is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("PrivateEquity", "PEQ") {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
 
-    /**
-    * Custom accessor to create a unique token
-    */
-    function mintUniqueTokenTo(
-        address _to,
-        uint256 _tokenId,
-        string  _tokenURI
-    ) public
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
     {
-        super._mint(_to, _tokenId);
-        super._setTokenURI(_tokenId, _tokenURI);
+        return super.tokenURI(tokenId);
     }
 }
