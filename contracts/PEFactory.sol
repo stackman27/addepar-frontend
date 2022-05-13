55c7557d62c1cc079262aff4a98500417798ddee
James Dai <jamesmdai@gmail.com>
2022-05-12 21:47:22 -0700

    added nftfactory pointer stuff

--- contracts/PEFactory.sol
+++ contracts/PEFactory.sol
@@ -5,9 +5,15 @@ import "./PrivateEquity.sol";
 contract PEFactory {
     // maps private equity node_id to its corresponding smart contract address
     mapping(uint256 => address) private private_equities;
+    // need to keep the address of the NFTFactory
+    address nftFactory;
+
+    constructor(address _nftFactory) {
+        nftFactory = _nftFactory;
+    }
 
     function addPrivateEquity (uint256 id) public returns (address){
-        PrivateEquity pe = new PrivateEquity(id);
+        PrivateEquity pe = new PrivateEquity(id, nftFactory);
         address addr = address(pe);
         private_equities[id] = addr;
         return addr;
--- contracts/PrivateEquity.sol
+++ contracts/PrivateEquity.sol
@@ -1,18 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "./NFTFactory.sol";
 
 contract PrivateEquity {
     uint256 id;
+    NFTFactory nftFactory;
     
-    constructor(uint256 _id) {
+    constructor(uint256 _id, address _nftFactory) {
         id = _id;
+        nftFactory = NFTFactory(_nftFactory);
     }
     
-    function contribute() public{
+    function contribute(uint256 amount) payable public{
+        require(msg.value == amount);
         return;
     }
 
-    function distribute() public {
+    function distribute(address payable to, uint256 amount) public {
+        to.transfer(amount);
         return;
     }
 }
